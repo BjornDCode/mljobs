d870a675e64e48e37ff662b45cba8f012a6a0dcb
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-12 15:36:02 +0200

    Sort jobs in descending order

--- tests/Feature/ViewJobsTest.php
+++ tests/Feature/ViewJobsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Job;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -20,10 +21,28 @@ class ViewJobsTest extends TestCase
 
         $response = $this->get('/');
         $response->assertStatus(200);
-        
+
         $this->assertTrue($response->data('jobs')->contains($jobA));
         $this->assertTrue($response->data('jobs')->contains($jobB));
         $this->assertTrue($response->data('jobs')->contains($jobC));
+
+    }
+
+    /** @test */
+    public function jobs_are_displayed_in_descending_order() 
+    {
+        $jobA = factory(Job::class)->create(['created_at' => Carbon::parse('-2 weeks')]);
+        $jobB = factory(Job::class)->create(['created_at' => Carbon::parse('-3 weeks')]);
+        $jobC = factory(Job::class)->create(['created_at' => Carbon::parse('-1 week')]);
+        
+        $response = $this->get('/');
+
+        // Assert the order is $jobC, $jobA, jobB
+        $this->assertEquals([
+            $jobC->id,
+            $jobA->id,
+            $jobB->id,
+        ], $response->data('jobs')->pluck('id')->values()->all());
     }
 
 }
