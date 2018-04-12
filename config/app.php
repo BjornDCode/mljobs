991efcac42b335c5be869c747376d7c237fc1d55
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-12 20:04:54 +0200

    Add svg blade directive

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        BladeSvg\BladeSvgServiceProvider::class,
         /*
          * Application Service Providers...
          */
