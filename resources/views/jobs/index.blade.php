6e3280e545994fea7051067e96b342f5a4fb448f
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-12 14:34:01 +0200

    Change default view

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Machine Learning Jobs') }}</title>
+
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    
+    <div id="app">
+        <h1>Hello World</h1>
+    </div>
+
+    <script src="js/app.js"></script>
+</body>
+</html>
