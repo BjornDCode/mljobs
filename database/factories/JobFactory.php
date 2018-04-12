f183e7a36263fc3438c16901f70438c74d5ff170
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-12 15:21:33 +0200

    Job factory

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Job;
+use Faker\Generator as Faker;
+
+$factory->define(Job::class, function (Faker $faker) {
+    return [
+        'title' => $faker->jobTitle,
+        'description' => $faker->text(1000),
+        'company' => $faker->company,
+        'company_logo' => 'http://placeholder.com/100x100',
+        'location' => $faker->city,
+        'salary' => '$110k',
+        'type' => 'Full Time',
+    ];
+});
