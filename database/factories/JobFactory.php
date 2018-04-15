c60f52375d6540d74c56d620d16ef2d500f964f3
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-15 14:32:23 +0200

    Update job factory to have multiple states

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -7,11 +7,17 @@ $factory->define(Job::class, function (Faker $faker) {
     return [
         'title' => $faker->jobTitle,
         'description' => $faker->text(1000),
+        'apply_url' => 'http://example.com'
+    ];
+});
+
+$factory->state(Job::class, 'full', function (Faker $faker) {
+    return [
+        'description' => $faker->text(1000),
         'company' => $faker->company,
         'company_logo' => 'http://via.placeholder.com/100x100',
         'location' => $faker->city,
         'salary' => '$110k',
         'type' => 'Full Time',
-        'apply_url' => 'http://example.com'
     ];
 });
