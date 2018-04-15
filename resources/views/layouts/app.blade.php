08f4b26984d8134bdb3d162d138cd84c4039e35d
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-15 19:01:22 +0200

    Add header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,19 @@
 <body>
     
     <div id="app">
+        <header>
+            <a class="logo" href="/">
+                @svg('logo')
+            </a>
+            <nav>
+                <a href="/" class="{{ (request()->input('filter') === null) ? 'active' : '' }}">All</a>
+                <a href="/?filter=full-time" class="{{ (request()->input('filter') === 'full-time') ? 'active' : '' }}">Full Time</a>
+                <a href="/?filter=part-time" class="{{ (request()->input('filter') === 'part-time') ? 'active' : '' }}">Part Time</a>
+                <a href="/?filter=internship" class="{{ (request()->input('filter') === 'internship') ? 'active' : '' }}">Internship</a>
+                <a href="/?filter=freelance" class="{{ (request()->input('filter') === 'freelance') ? 'active' : '' }}">Freelance</a>
+                <a href="/?filter=temporary" class="{{ (request()->input('filter') === 'temporary') ? 'active' : '' }}">Temporary</a>
+            </nav>
+        </header>
         @yield('content')
     </div>
 
