38ec0a30777622640e2876fa35b4f8bc05d0c8aa
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-15 14:32:45 +0200

    Update job seeder to create more realistic data

--- database/seeds/JobsTableSeeder.php
+++ database/seeds/JobsTableSeeder.php
@@ -8,7 +8,28 @@ class JobsTableSeeder extends Seeder
     
     public function run()
     {
-        factory(Job::class, 20)->create();    
+        factory(Job::class, 5)->states('full')->create();    
+
+        factory(Job::class)->states('full')->create([
+            'company' => null
+        ]);
+
+        factory(Job::class)->states('full')->create([
+            'company_logo' => null
+        ]);
+
+        factory(Job::class)->states('full')->create([
+            'location' => null
+        ]);
+
+        factory(Job::class)->states('full')->create([
+            'salary' => null
+        ]);
+
+        factory(Job::class)->states('full')->create([
+            'type' => null
+        ]);
+
     }
 
 }
