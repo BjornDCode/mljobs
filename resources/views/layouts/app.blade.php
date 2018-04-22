b02bd8385fae7875dfb8bbc8813ea0b6eaa32237
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-22 18:11:24 +0200

    Move navigation to blade partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,7 @@
             <a class="logo" href="/">
                 @svg('logo')
             </a>
-            <nav>
-                <a href="/" class="{{ (request()->input('filter') === null) ? 'active' : '' }}">All</a>
-                <a href="/?filter=full-time" class="{{ (request()->input('filter') === 'full-time') ? 'active' : '' }}">Full Time</a>
-                <a href="/?filter=part-time" class="{{ (request()->input('filter') === 'part-time') ? 'active' : '' }}">Part Time</a>
-                <a href="/?filter=internship" class="{{ (request()->input('filter') === 'internship') ? 'active' : '' }}">Internship</a>
-                <a href="/?filter=freelance" class="{{ (request()->input('filter') === 'freelance') ? 'active' : '' }}">Freelance</a>
-                <a href="/?filter=temporary" class="{{ (request()->input('filter') === 'temporary') ? 'active' : '' }}">Temporary</a>
-            </nav>
+            @include('partials.navigation')
         </header>
         @yield('content')
     </div>
