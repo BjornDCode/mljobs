883b97fadc8cf1084f830a4fa28467cbe02fbad4
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-04-22 19:15:36 +0200

    Newsletter form functionality

--- resources/assets/js/components/NewsletterForm.vue
+++ resources/assets/js/components/NewsletterForm.vue
@@ -2,15 +2,54 @@
     <div class="newsletter">
         <h2>Get the latest jobs in your inbox!</h2>
         <p>Receive a weekly email with the most interesting positions. We promise not to spam you!</p>
-        <form action="">
-            <input class="input" type="email" placeholder="Email">
-            <button class="button" type="submit">Subscribe</button>
+        <form @submit.prevent="submit">
+            <input 
+                class="input" 
+                type="email" 
+                placeholder="Email"
+                :disabled="successful"
+                v-model="email"
+            >
+            <button 
+                class="button" 
+                :class="{'loading': loading, 'successful': successful}" 
+                :disabled="loading || successful" 
+                type="submit"
+            >
+                Subscribe
+            </button>
         </form>
+        <p class="error" v-if="error">An error occurred</p>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                email: '',
+                error: false,
+                loading: false,
+                successful: false
+            }
+        },
 
+        methods: {
+            submit() {
+                this.loading = true;
+
+                axios.post('/newsletter', { email: this.email })
+                    .then(response => {
+                        this.loading = false;
+                        this.successful = true;
+                        this.error = false;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        this.successful = false;
+                        this.error = true;
+                    })
+            }
+        }
     }
 </script>
