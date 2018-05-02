0ed3a6275e3854661aa90fde84e0d03573a485a3
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-05-02 18:12:10 +0200

    Handle failed charges in gateway

--- app/StripeGateway.php
+++ app/StripeGateway.php
@@ -2,21 +2,31 @@
 
 namespace App;
 
+use Stripe\Charge;
+use Stripe\Stripe;
+use App\Exceptions\PaymentFailedException;
+
 class StripeGateway 
 {
 
     public function __construct() 
     {
-        \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
+        Stripe::setApiKey(config('services.stripe.secret'));
     }
 
     public function charge($token) 
     {
-        return \Stripe\Charge::create([
-            'amount' => 4900,
-            'currency' => 'usd',
-            'source' => $token
-        ]);
+        try {
+            $charge = Charge::create([
+                'amount' => 4900,
+                'currency' => 'usd',
+                'source' => $token
+            ]);
+        } catch (\Stripe\Error\Card | Exception $e) {
+            throw new PaymentFailedException;
+        }
+
+        return $charge;
     }
 
 }
--- tests/Integration/StripeGatewayTest.php
+++ tests/Integration/StripeGatewayTest.php
@@ -4,6 +4,7 @@ namespace Tests\Integration;
 
 use Tests\TestCase;
 use App\StripeGateway;
+use App\Exceptions\PaymentFailedException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class StripeGatewayTest extends TestCase
@@ -25,11 +26,7 @@ class StripeGatewayTest extends TestCase
 
         $charge = $stripeGateway->charge($validToken);
 
-        try {
-            $this->assertEquals($charge->id, \Stripe\Charge::retrieve($charge->id)->id);
-        } catch (Exception $e) {
-            $this->fail('Expected to see a Stripe charge, but did not');            
-        }
+        $this->assertEquals($charge->id, \Stripe\Charge::retrieve($charge->id)->id);
     }
 
     /** @test */
@@ -37,11 +34,19 @@ class StripeGatewayTest extends TestCase
     {
         $stripeGateway = new StripeGateway;
 
-        // $token = invalidToken
+        $invalidToken = \Stripe\Token::create([
+            'card' => [
+                'number' => '4000000000000002',
+                'exp_month' => 1,
+                'exp_year' => 2020,
+                'cvc' => 123
+            ]
+        ]);
+
+        $this->expectException(PaymentFailedException::class);
 
         $charge = $stripeGateway->charge($invalidToken);
 
-        // Assert exception was thrown?
     }
 
 }
