a82e4219a7b7b0d63b84053e68e9ad58a600a631
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-05-12 11:09:16 +0200

    Move filters to partial

---
+++ resources/views/jobs/_filters.blade.php
@@ -0,0 +1,8 @@
+<nav class="filters">
+    <a href="/" class="{{ (request()->input('filter') === null) ? 'active' : '' }}">All Jobs</a>
+    <a href="/?filter=full-time" class="{{ (request()->input('filter') === 'full-time') ? 'active' : '' }}">Full Time</a>
+    <a href="/?filter=part-time" class="{{ (request()->input('filter') === 'part-time') ? 'active' : '' }}">Part Time</a>
+    <a href="/?filter=internship" class="{{ (request()->input('filter') === 'internship') ? 'active' : '' }}">Internship</a>
+    <a href="/?filter=freelance" class="{{ (request()->input('filter') === 'freelance') ? 'active' : '' }}">Freelance</a>
+    <a href="/?filter=temporary" class="{{ (request()->input('filter') === 'temporary') ? 'active' : '' }}">Temporary</a>
+</nav>
